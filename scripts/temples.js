314650ae0814a19ac21615d2ecb977fc29980f1c
Brian Madroñero <brainmadro@gmail.com>
2024-05-20 18:40:16 -0500

    feat: add temple album

---
+++ scripts/temples.js
@@ -0,0 +1,233 @@
+const oldTemples = [
+  {
+    src: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/accra-ghana/1200x675/accra-ghana-temple-detail-249022-2400x1200.jpg",
+    alt: "Accra Ghana Temple",
+    caption: "An exterior view of the Accra Ghana Temple during the day",
+  },
+  {
+    src: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/accra-ghana/1200x675/accra-ghana-temple-detail-249022-2400x1200.jpg",
+    alt: "Accra Ghana Temple",
+    caption: "An exterior view of the Accra Ghana Temple during the day",
+  },
+  {
+    src: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/accra-ghana/1200x675/accra-ghana-temple-detail-249022-2400x1200.jpg",
+    alt: "Accra Ghana Temple",
+    caption: "An exterior view of the Accra Ghana Temple during the day",
+  },
+  {
+    src: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/accra-ghana/1200x675/accra-ghana-temple-detail-249022-2400x1200.jpg",
+    alt: "Accra Ghana Temple",
+    caption: "An exterior view of the Accra Ghana Temple during the day",
+  },
+  {
+    src: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/accra-ghana/1200x675/accra-ghana-temple-detail-249022-2400x1200.jpg",
+    alt: "Accra Ghana Temple",
+    caption: "An exterior view of the Accra Ghana Temple during the day",
+  },
+  {
+    src: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/accra-ghana/1200x675/accra-ghana-temple-detail-249022-2400x1200.jpg",
+    alt: "Accra Ghana Temple",
+    caption: "An exterior view of the Accra Ghana Temple during the day",
+  },
+  {
+    src: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/accra-ghana/1200x675/accra-ghana-temple-detail-249022-2400x1200.jpg",
+    alt: "Accra Ghana Temple",
+    caption: "An exterior view of the Accra Ghana Temple during the day",
+  },
+  {
+    src: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/accra-ghana/1200x675/accra-ghana-temple-detail-249022-2400x1200.jpg",
+    alt: "Accra Ghana Temple",
+    caption: "An exterior view of the Accra Ghana Temple during the day",
+  },
+  {
+    src: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/accra-ghana/1200x675/accra-ghana-temple-detail-249022-2400x1200.jpg",
+    alt: "Accra Ghana Temple",
+    caption: "An exterior view of the Accra Ghana Temple during the day",
+  },
+];
+const newTemples = [
+  {
+    src: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/albuquerque-new-mexico/1280x800/albuquerque-temple-lds-137883-wallpaper.jpg",
+    alt: "Albuquerque New Mexico Temple",
+    caption: "Albuquerque New Mexico Temple",
+  },
+  {
+    src: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/albuquerque-new-mexico/1280x800/albuquerque-temple-lds-137883-wallpaper.jpg",
+    alt: "Albuquerque New Mexico Temple",
+    caption: "Albuquerque New Mexico Temple",
+  },
+  {
+    src: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/albuquerque-new-mexico/1280x800/albuquerque-temple-lds-137883-wallpaper.jpg",
+    alt: "Albuquerque New Mexico Temple",
+    caption: "Albuquerque New Mexico Temple",
+  },
+  {
+    src: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/albuquerque-new-mexico/1280x800/albuquerque-temple-lds-137883-wallpaper.jpg",
+    alt: "Albuquerque New Mexico Temple",
+    caption: "Albuquerque New Mexico Temple",
+  },
+  {
+    src: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/albuquerque-new-mexico/1280x800/albuquerque-temple-lds-137883-wallpaper.jpg",
+    alt: "Albuquerque New Mexico Temple",
+    caption: "Albuquerque New Mexico Temple",
+  },
+  {
+    src: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/albuquerque-new-mexico/1280x800/albuquerque-temple-lds-137883-wallpaper.jpg",
+    alt: "Albuquerque New Mexico Temple",
+    caption: "Albuquerque New Mexico Temple",
+  },
+  {
+    src: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/albuquerque-new-mexico/1280x800/albuquerque-temple-lds-137883-wallpaper.jpg",
+    alt: "Albuquerque New Mexico Temple",
+    caption: "Albuquerque New Mexico Temple",
+  },
+  {
+    src: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/albuquerque-new-mexico/1280x800/albuquerque-temple-lds-137883-wallpaper.jpg",
+    alt: "Albuquerque New Mexico Temple",
+    caption: "Albuquerque New Mexico Temple",
+  },
+  {
+    src: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/albuquerque-new-mexico/1280x800/albuquerque-temple-lds-137883-wallpaper.jpg",
+    alt: "Albuquerque New Mexico Temple",
+    caption: "Albuquerque New Mexico Temple",
+  },
+];
+const largeTemples = [
+  {
+    src: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/arequipa-peru/1280x800/4-48661c257177c19a0f39a3991b1a7e7aa0338487.jpeg",
+    alt: "Arequipa Peru Temple",
+    caption: "Exterior of the Arequipa Peru Temple",
+  },
+  {
+    src: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/arequipa-peru/1280x800/4-48661c257177c19a0f39a3991b1a7e7aa0338487.jpeg",
+    alt: "Arequipa Peru Temple",
+    caption: "Exterior of the Arequipa Peru Temple",
+  },
+  {
+    src: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/arequipa-peru/1280x800/4-48661c257177c19a0f39a3991b1a7e7aa0338487.jpeg",
+    alt: "Arequipa Peru Temple",
+    caption: "Exterior of the Arequipa Peru Temple",
+  },
+  {
+    src: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/arequipa-peru/1280x800/4-48661c257177c19a0f39a3991b1a7e7aa0338487.jpeg",
+    alt: "Arequipa Peru Temple",
+    caption: "Exterior of the Arequipa Peru Temple",
+  },
+  {
+    src: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/arequipa-peru/1280x800/4-48661c257177c19a0f39a3991b1a7e7aa0338487.jpeg",
+    alt: "Arequipa Peru Temple",
+    caption: "Exterior of the Arequipa Peru Temple",
+  },
+  {
+    src: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/arequipa-peru/1280x800/4-48661c257177c19a0f39a3991b1a7e7aa0338487.jpeg",
+    alt: "Arequipa Peru Temple",
+    caption: "Exterior of the Arequipa Peru Temple",
+  },
+  {
+    src: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/arequipa-peru/1280x800/4-48661c257177c19a0f39a3991b1a7e7aa0338487.jpeg",
+    alt: "Arequipa Peru Temple",
+    caption: "Exterior of the Arequipa Peru Temple",
+  },
+  {
+    src: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/arequipa-peru/1280x800/4-48661c257177c19a0f39a3991b1a7e7aa0338487.jpeg",
+    alt: "Arequipa Peru Temple",
+    caption: "Exterior of the Arequipa Peru Temple",
+  },
+  {
+    src: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/arequipa-peru/1280x800/4-48661c257177c19a0f39a3991b1a7e7aa0338487.jpeg",
+    alt: "Arequipa Peru Temple",
+    caption: "Exterior of the Arequipa Peru Temple",
+  },
+];
+const smallTemples = [
+  {
+    src: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/boise-idaho/1280x800/boise-idaho-temple-exterior-2012-1029111-wallpaper.jpg",
+    alt: "Boise Idaho Temple",
+    caption: "The exterior of the newly-remodeled Boise Idaho Temple, 2012.",
+  },
+  {
+    src: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/boise-idaho/1280x800/boise-idaho-temple-exterior-2012-1029111-wallpaper.jpg",
+    alt: "Boise Idaho Temple",
+    caption: "The exterior of the newly-remodeled Boise Idaho Temple, 2012.",
+  },
+  {
+    src: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/boise-idaho/1280x800/boise-idaho-temple-exterior-2012-1029111-wallpaper.jpg",
+    alt: "Boise Idaho Temple",
+    caption: "The exterior of the newly-remodeled Boise Idaho Temple, 2012.",
+  },
+  {
+    src: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/boise-idaho/1280x800/boise-idaho-temple-exterior-2012-1029111-wallpaper.jpg",
+    alt: "Boise Idaho Temple",
+    caption: "The exterior of the newly-remodeled Boise Idaho Temple, 2012.",
+  },
+  {
+    src: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/boise-idaho/1280x800/boise-idaho-temple-exterior-2012-1029111-wallpaper.jpg",
+    alt: "Boise Idaho Temple",
+    caption: "The exterior of the newly-remodeled Boise Idaho Temple, 2012.",
+  },
+  {
+    src: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/boise-idaho/1280x800/boise-idaho-temple-exterior-2012-1029111-wallpaper.jpg",
+    alt: "Boise Idaho Temple",
+    caption: "The exterior of the newly-remodeled Boise Idaho Temple, 2012.",
+  },
+  {
+    src: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/boise-idaho/1280x800/boise-idaho-temple-exterior-2012-1029111-wallpaper.jpg",
+    alt: "Boise Idaho Temple",
+    caption: "The exterior of the newly-remodeled Boise Idaho Temple, 2012.",
+  },
+  {
+    src: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/boise-idaho/1280x800/boise-idaho-temple-exterior-2012-1029111-wallpaper.jpg",
+    alt: "Boise Idaho Temple",
+    caption: "The exterior of the newly-remodeled Boise Idaho Temple, 2012.",
+  },
+  {
+    src: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/boise-idaho/1280x800/boise-idaho-temple-exterior-2012-1029111-wallpaper.jpg",
+    alt: "Boise Idaho Temple",
+    caption: "The exterior of the newly-remodeled Boise Idaho Temple, 2012.",
+  },
+];
+const menu$ = document.querySelector(".menu-content");
+const navbar$ = document.querySelector("nav");
+const sectionTitle$ = document.querySelector("#sectionTitle");
+const imagesContainer$ = document.querySelector("#templeImages");
+const hamburger$ = document.querySelector(".hamburger");
+
+hamburger$.addEventListener("click", function () {
+  hamburger$.classList.toggle("isactive");
+  navbar$.classList.toggle("active");
+});
+
+for (const element of menu$.children) {
+  element.addEventListener("click", (event) => {
+    const category = event.target.dataset.category;
+    imagesContainer$.innerHTML = "";
+    populateGallery(category);
+    hamburger$.classList.remove("isactive");
+    navbar$.classList.remove("active");
+  });
+}
+
+function populateGallery(category) {
+  if (category == "home") {
+    return (sectionTitle$.innerHTML =
+      category.charAt(0).toUpperCase() + category.slice(1).toLowerCase());
+  }
+
+  const temples = {
+    old: oldTemples,
+    new: newTemples,
+    large: largeTemples,
+    small: smallTemples,
+  };
+
+  sectionTitle$.innerHTML =
+    category.charAt(0).toUpperCase() + category.slice(1).toLowerCase();
+
+  if (imagesContainer$) {
+    temples[category].forEach((img) => {
+      imagesContainer$.innerHTML += `<figure><img loading="lazy" src="${img.src}" alt="${img.alt}"><figcaption>${img.caption}</figcaption></figure>`;
+    });
+  }
+}
+
+populateGallery("home");
